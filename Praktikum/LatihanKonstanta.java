af7572430ae9ea071a18431f575701d4099a4c0a
asyahra19 <90130931+asyahra19@users.noreply.github.com>
2022-09-07 20:46:20 +0700

    Add files via upload

---
+++ Praktikum/LatihanKonstanta.java
@@ -0,0 +1,10 @@
+public class LatihanKonstanta {
+    public static final int KURS_DOLLAR = 15000;
+    public static final double PI = 3.14;
+    public static final String NAMA = "Squidward";
+    public static void main(String args[]) {
+        System.out.println(KURS_DOLLAR);
+        System.out.println(PI);
+        System.out.println(NAMA);
+    }
+}
