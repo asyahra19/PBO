feb7dc7696dd8999ebdeb1df8a989f14c574489e
asyahra19 <90130931+asyahra19@users.noreply.github.com>
2022-09-22 19:51:10 +0700

    Add files via upload

---
+++ Praktikum/ForLoopDemo.java
@@ -0,0 +1,11 @@
+public class ForLoopDemo {
+    public static void main(String[] args) {
+        int arr[] = {5, 1, 4, 2, 3};
+        /*for (int i = 1; i < 6; i++) {
+            System.out.println("elemen ke-" + i + " = " + arr[i]);*/
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("elemen ke-" + i + " = " + arr[i]);
+        }
+    }
+}
